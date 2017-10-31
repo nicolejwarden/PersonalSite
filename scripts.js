a79a2f78be7f9d841a1d374490ff1e6aab54b62c
nicolejwarden <30838190+nicolejwarden@users.noreply.github.com>
2017-10-31 10:52:52 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//This will be a place to store JavaScript scripts for webpage
+
+
+
+
+
+//on Nav Search Click
+<script> 
+function alertBox() {
+    document.getElementById("submit").alert = "This button doesn't do anything...";
+}
+</script>
